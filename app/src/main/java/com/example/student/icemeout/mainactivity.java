63f22f08fab45d3c5db5a16c5c5511003b76f890
Student <student@Admins-MacBook-Air.local>
2018-12-08 15:38:56 -0800

    Initial commit

---
+++ app/src/main/java/com/example/student/icemeout/mainactivity.java
@@ -0,0 +1,13 @@
+package com.example.student.icemeout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class mainactivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainactivity);
+    }
+}
